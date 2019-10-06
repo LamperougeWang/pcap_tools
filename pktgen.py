110740bc3ad3a93c2c8be76f6fb341e24b367bf4
WangSC <wscthu2015@gmail.com>
2019-10-06 10:12:55 +0800

    Update pktgen.py
    delete sensitive data

--- pktgen.py
+++ pktgen.py
@@ -18,8 +18,8 @@ def send_trans_pkt(eth_src, eth_dst, ip_src, ip_dst, proto, port_src, port_dst, 
 
 def main():
     for i in range(0, 2000):
-    	send_trans_pkt('68:91:d0:61:b4:c5', '68:91:d0:61:12:5a', '10.0.0.3', '10.0.0.4', 'TCP', 100, 99, 10, 'p1p1')
-#    	send_trans_pkt('14:18:77:53:80:3e', '90:e2:ba:01:1d:99', '10.0.0.1', '101.6.30.6', 'TCP', 100, 99, 10, 'eno4')
+    	send_trans_pkt('11:11:11:11:11:11', '22:22:22:22:22:22', '10.0.0.3', '10.0.0.4', 'TCP', 100, 99, 10, 'p1p1')
+
 
 if __name__ == '__main__':
 	main()
